1d4b992190fc40674b5a878fd269947648426bee
tiana30 <tiana3059@gmail.com>
2020-09-19 20:03:21 +0700

    fix code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import Datatable from './datatable';
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [q, setQ ] = useState("");
-  const ApiUrl = "https://gorest.co.in/public-api/users";
-  useEffect(() => {
-    fetch(ApiUrl)
-    .then((response) => response.json())
-    .then(responseJson => {
-      if(responseJson.data) {
-          return Promise.resolve(setData(responseJson.data)
-        );
-          
-      } else {
-          return Promise.reject(`data is not found`);
-      }
-    });
-    
-  }, []);
-  
-  function searchUser(rows) {
-    const columns = data[0] && Object.keys(data[0]);
-
-    return rows.filter(row => 
-     columns.some((column) => 
-     row[column].toString().toLowerCase().indexOf(q) > -1 
-     )
-      )
-  }
 
   return (
-    <div>
-    <div><h6>Filter Data</h6>
-    <input type="text" 
-    value={q} 
-    onChange={(e) => setQ(e.target.value)}/>
-    </div>
-    <Datatable data = {searchUser(data)} />
-      
-    </div>
+    <>
+    </>
   );
 }
 
