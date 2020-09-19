02e01d4a7535313995143114ef365d2ed6366c31
tiana30 <tiana3059@gmail.com>
2020-09-19 19:49:57 +0700

    filter user by object column

--- src/App.js
+++ src/App.js
@@ -22,11 +22,12 @@ export default function App() {
   }, []);
   
   function searchUser(rows) {
+    const columns = data[0] && Object.keys(data[0]);
+
     return rows.filter(row => 
-      row.name.toLowerCase().indexOf(q) > -1 || 
-      row.email.toLowerCase().indexOf(q) > -1 ||
-      row.gender.toLowerCase().indexOf(q) > -1 ||
-      row.status.toLowerCase().indexOf(q) > -1
+     columns.some((column) => 
+     row[column].toString().toLowerCase().indexOf(q) > -1 
+     )
       )
   }
 
