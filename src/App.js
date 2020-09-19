be8c6c91fa7b0de810e6919d8c6fcb027d2bb2af
tiana30 <tiana3059@gmail.com>
2020-09-19 18:32:10 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [q, setQ ] = useState("");
+  const ApiUrl = "https://gorest.co.in/public-api/users";
+  useEffect(() => {
+    fetch(ApiUrl)
+    .then((response) => response.json())
+    .then(responseJson => {
+      if(responseJson.data) {
+          return Promise.resolve(setData(responseJson.data)
+        );
+          
+      } else {
+          return Promise.reject(`data is not found`);
+      }
+    });
+    
+  }, []);
+  console.log(data);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Master Percobaan</h1>
-      </header>
+    <div>
+    <div>This is for filter</div>
+    <div>This is for datatables</div>
+      
     </div>
   );
 }
 
-export default App;
